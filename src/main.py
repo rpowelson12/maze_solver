6d485c78b85e6d925f0d4b2c6e3dfce66748982e
rpowelson12 <randy.powelson@yahoo.com>
2025-04-13 12:36:11 -0700

    drawing lines

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from window import Window
+from point import Point
+from line import Line
 
 win = Window(800, 600)
+point1 = Point(100, 100)
+point2 = Point(200, 200)
+line = Line(point1, point2)
+win.draw_line(line, "red")
+
 win.wait_for_close()
