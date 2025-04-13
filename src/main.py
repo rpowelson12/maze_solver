a55aac61ea95d3ea8e8ff8ee7e51fd0ea3081ae9
rpowelson12 <randy.powelson@yahoo.com>
2025-04-13 14:01:35 -0700

    refactor

--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
-from window import Window
-from point import Point
-from line import Line
+from graphics import Window
+from cell import Cell
 
-win = Window(800, 600)
-point1 = Point(100, 100)
-point2 = Point(200, 200)
-line = Line(point1, point2)
-win.draw_line(line, "red")
 
-win.wait_for_close()
+def main():
+    win = Window(800, 600)
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
+    win.wait_for_close()
+
+
+main()
