1c82c26a490a691176ca4133842eeb7bb2190445
rpowelson12 <randy.powelson@yahoo.com>
2025-04-13 19:55:36 -0700

    maze drawn

--- src/cell.py
+++ src/cell.py
@@ -30,3 +30,23 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        if undo == False:
+            color = "red"
+        else:
+            color = "gray"
+        
+        # Calculate center point of current cell
+        x1_mid = (self._x1 + self._x2) / 2
+        y1_mid = (self._y1 + self._y2) / 2
+        
+        # Calculate center point of to_cell
+        x2_mid = (to_cell._x1 + to_cell._x2) / 2
+        y2_mid = (to_cell._y1 + to_cell._y2) / 2
+        
+        # Create and draw the line
+        line = Line(Point(x1_mid, y1_mid), Point(x2_mid, y2_mid))
+        # You may need to set the color property on the line object
+        # For example: line.set_fill(color)
+        self._win.draw_line(line, color)
